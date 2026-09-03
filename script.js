a41185cef33bc81f39d74a45e120bca4f6062449
Iconoclastics <kirillpapihin@gmail.com>
2026-09-03 18:41:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function changeText() {
-    document.getElementById("text").textContent = "Ты только что изменил сайт с помощью JavaScript!";
+    const text = document.getElementById("text");
+
+    text.style.opacity = "0";
+
+    setTimeout(function() {
+        text.textContent = "Ты только что изменил сайт с помощью JavaScript!";
+        text.style.opacity = "1";
+    }, 400);
 }
